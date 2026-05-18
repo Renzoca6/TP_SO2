b1c35e3254d77723d41dd62dfccfe59cc1e55632
Renzo <renzocariseo@gmail.com>
2026-05-18 01:40:59 +0000

    feat testprio

--- x64BareBones/Kernel/syscall/syscall.c
+++ x64BareBones/Kernel/syscall/syscall.c
@@ -466,6 +466,7 @@ static void syscall_block(uint64_t *registers) {
         pcb->state = BLOCKED;
         yield_process();
     } else if (pcb && pcb->state == READY) {
+        remove_from_ready_queue(pcb);
         pcb->state = BLOCKED;
     }
     registers[14] = 0;
